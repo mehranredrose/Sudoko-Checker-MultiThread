4f16c519e93f0770a89b10c4c752d92dc74fd5dd
Mehran RedRose <mehranredrose@gmail.com>
2021-02-12 23:22:19 +0330

    removing some comments

--- sudoko.py
+++ sudoko.py
@@ -1,3 +1,4 @@
+#mehranredrose 
 import numpy as np
 import copy
 import sys
@@ -36,25 +37,6 @@ def truedigit(): #gets only digits not strings or others
     number=int(number)
     return number
 
-#def possible (row ,column,number):
-#    global grid
-#    #is number apearing in the given row?
-#    for i in range(0,9):
-#        if grid[row][i] == number :
-#            return False
-#    #is number apearing in the given column?
-#    for i in range(0,9):
-#        if grid[i][column] == number :
-#            return False
-#    #is number apearing in the given square?
-#    section_x = (column//3) * 3 #it devide the main square into 3 section in x   
-#    section_y = (row//3) * 3 #it devide the main Square into 3 section in y
-#    for i in range(0,3):#for 3 rows of each square
-#        for j in range(0,3):#for 3 columns of each square :)
-#          if grid[i+ section_y][j + section_x]== number :
-#               return False
-#
-#    return True 
 
 def possible_row(row,number):#is number apearing in the given row?
     for i in range(0,9):
